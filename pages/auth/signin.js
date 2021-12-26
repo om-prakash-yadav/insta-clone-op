793727b2e30e874de5f9b3e64e5d11d2f8970301
om-prakash-yadav <jnvomprakash121@gmail.com>
2021-12-26 22:01:46 +0530

    ui improvement

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -1,4 +1,5 @@
 import { getProviders, signIn as SignIntoProvider } from "next-auth/react";
+import Image from "next/image";
 import Header from "../../components/Header";
 
 function signIn({ providers }) {
@@ -6,19 +7,21 @@ function signIn({ providers }) {
     <>
       <Header />
       <div className="flex flex-col items-center justify-center min-h-screen py-2 md:-mt-32 -mt-20 px-14 text-center">
-        <img
-          className="w-80"
+        <Image
+          height={120}
+          width={300}
+          layout="fixed"
           src="https://raw.githubusercontent.com/om-prakash-yadav/links/main/instagram-logo.png"
-          alt=""
+          alt="instagram" 
         />
         <p className="text-xs md:text-sm italic text-gray-400">
           Sign up to see photos and videos from your friends
         </p>
-        <div className="mt-40 ">
+        <div className="mt-32 ">
           {Object.values(providers).map((provider) => (
             <div key={provider.name}>
               <button
-                className="p-1 px-3 md:py-3 bg-blue-500 text-white rounded-full"
+                className="p-1 px-3 md:py-3 bg-blue-500 text-white rounded-full focus:ring-2 ring-blue-300"
                 onClick={() =>
                   SignIntoProvider(provider.id, { callbackUrl: "/" })
                 }
